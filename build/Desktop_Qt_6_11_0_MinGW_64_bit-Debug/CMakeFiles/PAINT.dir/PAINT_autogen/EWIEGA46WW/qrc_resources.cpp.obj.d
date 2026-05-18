c6f724dd409b8d1b834f1409dddc2f3d0ad97fd4
Tengku Hafiz Novandi <hafiznovandi200726@gmail.com>
2026-05-18 21:31:30 +0700

    Delete build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/PAINT.dir/PAINT_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/PAINT.dir/PAINT_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- C:\Users\HP\Downloads\paint_app_upgraded\ (7)\paint_app\build\Desktop_Qt_6_11_0_MinGW_64_bit-Debug\PAINT_autogen\EWIEGA46WW\qrc_resources.cpp
